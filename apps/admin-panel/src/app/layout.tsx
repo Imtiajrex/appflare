0e44b6fd2479de448cf0795830af6f826b60e495
Imtiaj Bin Aoual <imtiajrex@gmail.com>
2025-02-16 01:05:54 +0600

    fixed type issues and added top level suspense

--- apps/admin-panel/src/app/layout.tsx
+++ apps/admin-panel/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
 import './globals.css'
 import { Provider } from './provider'
+import { Suspense } from 'react'
+import { Icon } from '@iconify/react/dist/iconify.js'
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -28,10 +30,21 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} light bg-gray-50 antialiased dark:bg-gray-950`}
       >
-        <Provider>
-          {children}
-          <Toaster position="top-center" reverseOrder={false} />
-        </Provider>
+        <Suspense
+          fallback={
+            <div className="flex h-screen w-screen items-center justify-center">
+              <Icon
+                icon={'tabler:circle-dashed'}
+                className="text-tremor-brand h-10 w-10 animate-spin"
+              />
+            </div>
+          }
+        >
+          <Provider>
+            {children}
+            <Toaster position="top-center" reverseOrder={false} />
+          </Provider>
+        </Suspense>
       </body>
     </html>
   )
