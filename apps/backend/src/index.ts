5ae537eebfa1a53f6c5a43a0c258f437d8760540
Imtiaj <imtiajrex@gmail.com>
2025-08-02 00:29:51 +0600

    updated better-auth and mongodb connection for backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,30 +1,33 @@
-import { server, DBObject } from '@appflare/server'
+import { server, DBObject } from 'appflare/server'
 declare global {
-  interface Env {
+  type AppflareEnv = {
     MONGO_URI: string
+    MONGO_DB: string
     BUCKET?: R2Bucket
     KV?: KVNamespace
-    DURABLE_OBJECT?: DurableObjectNamespace<DBObject>
+    DURABLE_OBJECT?: DurableObjectNamespace
+    STRIPE_SECRET_KEY: string
+    STRIPE_SIGNING_SECRET: string
+    CF?: CfProperties<any>
   }
   interface CF extends Context {
-    env: Env
+    env: AppflareEnv
     ctx: ExecutionContext
   }
   interface Context {
-    env: Env
+    env: AppflareEnv
     ctx: ExecutionContext
   }
 }
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  fetch: async (request: Request, env: Env) => {
+  fetch: async (request: Request, env: AppflareEnv, cf: CfProperties<any>) => {
     return server(request, {
+      ...env,
       MONGO_URI: env.MONGO_URI,
-      BUCKET: env.BUCKET,
-      KV: env.KV,
-      DURABLE_OBJECT: env.DURABLE_OBJECT,
+      CF: cf,
     })
   },
 }
-export { DBObject }
+export { DBObject as MongoDurableObject }
